01b8a4b1d53745ab6f7dcad2aefdd75ed95dfa58
Dana <dkbuchshtav@gmail.com>
2025-11-29 15:57:42 +0100

    initial extention

---
+++ li-unhecker/content_script.js
@@ -0,0 +1,3 @@
+// Put all the javascript code here, that you want to execute after page load.
+document.querySelectorAll('.job-card-container').forEach(el => 
+    {el.style.border = "2px solid blue";});
